c589c7695e9a060a14966e3657eda6daaa1883b3
LEÓN ALEXIS BUITRAGO LÓPEZ - ANALISTA CONSULTORÍA <labuitrago@cidet.org.co>
2021-06-12 11:22:23 -0500

    Proyecto_Web_ML

---
+++ ML_Proyecto.py
@@ -0,0 +1,9 @@
+data = [1, 2, 3, 4, 5]
+df = []
+for i in data:
+    print (i/2)
+    df.append(i/4)
+
+print(f"El df i/4 {df}")
+
+
--- Proyecto_Web.py
+++ Proyecto_Web.py
@@ -8,4 +8,10 @@ print (c)
 
 print ("Hola mundo")
 
-g = 3
+g = [3, 3, 5, 7]
+
+for i in g:
+    print (i)
+
+print ("Code")
+
